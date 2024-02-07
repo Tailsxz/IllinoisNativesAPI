6aa17c5b7f7970326b7f1f72af4e0e2761387aa2
Tai Phan <ptai7722@gmail.com>
2024-02-06 21:05:43 -0600

    successful migration to mongoDB, nice refresher on how to use the base mongo drivers

--- server.js
+++ server.js
@@ -20,51 +20,51 @@ async function startDB() {
     db = client.db('ILNatives');
     collection = db.collection('plants');
 
-    addPlants();
   } catch(err) {
     console.error(err);
   }
 }
 startDB();
 
-async function addPlants() {
-  try {
-    const insertManyRes = await collection.insertMany(plants);
-    console.log(insertManyRes.insertedCount, 'documents inserted.')
-  } catch (err) {
-    console.error(err);
-  }
-};
-  
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-app.get('/plants/byId/:id', (request, response) => {
-  const requestId = Number(request.params?.id)
-  if (isNaN(requestId)) {
-    return response.status(404).json({error: 'Please only use numbers for the id path'});
+app.get('/plants/byId/:id', async (req, res) => {
+  const id = Number(req.params?.id)
+  if (isNaN(id)) {
+    return res.status(404).json({error: 'Please only use numbers for the id path'});
   }
-  if (plants.some(plant => plant.id === requestId)) {
-    response.json(plants[requestId - 1]);
-  } else {
-    response.status(404).json({error: 'Plant Not Found', message:'Sorry, the requested plant was not found. Please refer to our documentation in our github repo for more information.'})
+  try {
+    const plant = await collection.findOne({ id });
+    delete plant._id;
+    delete plant.id;
+
+    res.json(plant);
+    
+  } catch (err) {
+    console.error(err);
   }
+  // if (plants.some(plant => plant.id === reqId)) {
+  //   res.json(plants[reqId - 1]);
+  // } else {
+  //   res.status(404).json({error: 'Plant Not Found', message:'Sorry, the reqed plant was not found. Please refer to our documentation in our github repo for more information.'})
+  // }
 });
 
-app.get('/plants/byName/:name', (request, response) => {
-  //Decoding any spaces within the endpoint the client requested.
-  // const requestName = decodeURIComponent(request.params.name.toLowerCase().split(' ').join(''));
+app.get('/plants/byName/:name', (req, res) => {
+  //Decoding any spaces within the endpoint the client reqed.
+  // const reqName = decodeURIComponent(req.params.name.toLowerCase().split(' ').join(''));
   //sanitizing inputs
   const alphas = 'abcdefghijklmnopqrstuvwxyz'
-  const requestName = request.params.name.toLowerCase().split('').filter((char) => alphas.includes(char)).join('');
-  const plant = plants.find(plant => plant.lowerCaseAndRemoveSpace('commonName') === requestName);
+  const reqName = req.params.name.toLowerCase().split('').filter((char) => alphas.includes(char)).join('');
+  const plant = plants.find(plant => plant.lowerCaseAndRemoveSpace('commonName') === reqName);
 
-  console.log(requestName, plant);
+  console.log(reqName, plant);
 
   if (plant) {
-    response.json(plant);
+    res.json(plant);
   } else {
-    response.status(404).json({error: 'Plant Not Found', message:'Sorry, the requested plant was not found. Please refer to our documentation in our github repo for more information.'})
+    res.status(404).json({error: 'Plant Not Found', message:'Sorry, the requested plant was not found. Please refer to our documentation in our github repo for more information.'})
   }
 }) 
 
